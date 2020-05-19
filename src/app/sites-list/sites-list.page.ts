40371227bcaf16f47024a8363fb21acf497d01fd
Ryan Bertrand <ryan.bertrand@gmail.com>
2020-05-19 22:16:48 +0200

    liste des sites: infinite scroll

--- src/app/sites-list/sites-list.page.ts
+++ src/app/sites-list/sites-list.page.ts
@@ -1,6 +1,7 @@
-import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
+import {ChangeDetectorRef, Component, OnInit, ViewChild} from '@angular/core';
 import {SitesService} from '../services/sites.service';
 import {environment} from '../../environments/environment';
+import {IonInfiniteScroll} from '@ionic/angular';
 
 @Component({
   selector: 'app-sites-list',
@@ -8,8 +9,11 @@ import {environment} from '../../environments/environment';
   styleUrls: ['./sites-list.page.scss'],
 })
 export class SitesListPage implements OnInit {
+  @ViewChild(IonInfiniteScroll, {static: false}) infiniteScroll: IonInfiniteScroll;
 
   sites;
+
+  currentPage = 1;
 
   imgURL;
 
@@ -21,6 +25,20 @@ export class SitesListPage implements OnInit {
     this.sitesService.getSites().subscribe(res => {
       this.sites = res.records;
       this.cdr.detectChanges();
+    });
+  }
+
+  loadData() {
+    this.currentPage += 1;
+    console.log(this.currentPage);
+    this.infiniteScroll.disabled = true;
+    this.sitesService.getSites(this.currentPage).subscribe(res => {
+      console.log(res.records);
+      this.sites = this.sites.concat(res.records);
+      this.cdr.detectChanges();
+      if (res.records.length === 20) {
+        this.infiniteScroll.disabled = false;
+      }
     });
   }
 
